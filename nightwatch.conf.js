07f5c258fb1250bab7d86b5055df5c96b7521810
Sa3eed5 <111127373+Sa3eed5@users.noreply.github.com>
2025-07-09 02:42:56 +0300

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -4,6 +4,12 @@ module.exports = {
 
 
   page_objects_path: ["UI-test/page-objects"],
+  webdriver: {
+  start_process: true,
+  server_path: require('chromedriver').path,
+  port: 4444,
+},
+
 
   test_settings: {
     default: {
